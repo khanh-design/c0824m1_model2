bf291a0b772087470a772906fa391aa87445040c
khanh-design <buiduykhanh1062003@gmail.com>
2024-11-08 02:35:57 +0700

    Operator-Expression

---
+++ src/Operator_Expression/Rectangle.java
@@ -0,0 +1,21 @@
+package Operator_Expression;
+
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        float width;
+        float height;
+
+        Scanner sc = new Scanner(System.in); // Khai bao scan
+
+        System.out.println("Enter width: ");
+        width = sc.nextFloat();
+
+        System.out.println("Enter height: ");
+        height = sc.nextFloat();
+
+        float area = width * height;
+        System.out.println("Area is " + area);
+    }
+}
