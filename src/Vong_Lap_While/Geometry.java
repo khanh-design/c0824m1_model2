62e7d2e113157f6c76ff6f042ad7a44c57e5e15b
Khanh-design <buiduykhanh1062003@gmail.com>
2024-11-10 09:29:20 +0700

    Vong_Lap_While

---
+++ src/Vong_Lap_While/Geometry.java
@@ -0,0 +1,46 @@
+package Vong_Lap_While;
+
+import java.util.Scanner;
+
+public class Geometry {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Menu");
+        System.out.println("------------------------------");
+        System.out.println("1. Draw the triangle");
+        System.out.println("2. Draw the square");
+        System.out.println("3. Draw the rectangle");
+        System.out.println("0. Exit");
+        System.out.println("enter your choice");
+        int choice;
+        choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Draw the triangle");
+                for (int i = 0; i < 6; i++) {
+                    for (int j = 0; j < i - 1; j++) {
+                        System.out.print("*");
+                    }
+                    System.out.print("\n");
+                }
+                break;
+                case 2:
+                    System.out.println("Draw the square");
+                    for (int i = 0; i < 6; i++) {
+                        for (int j = 0; j < 6; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                    case 3:
+                        System.out.println("Draw the rectangle");
+                        for (int i = 0; i < 3; i++) {
+                            for (int j = 0; j < 6; j++) {
+                                System.out.print("*");
+                            }
+                            System.out.println();
+                        }
+        }
+    }
+}
